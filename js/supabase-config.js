fd5625c3ce0513600159ab4f10a0a3050fd0318e
ELITA-TECH <142341914+ELITA-TECH@users.noreply.github.com>
2026-06-02 00:26:12 +0200

    Add files via upload

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,8 +1,8 @@
 
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
 
-const supabaseUrl = 'WSTAW_SUPABASE_URL'
-const supabaseKey = 'WSTAW_SUPABASE_ANON_KEY'
+const supabaseUrl = 'https://xvbmhibtolynpmlajuin.supabase.co/rest/v1/'
+const supabaseKey = 'sb_publishable_xRAGkvzfhFz76nx7L9WbRQ_IzIBL1z2'
 
 export const supabase = createClient(
   supabaseUrl,
